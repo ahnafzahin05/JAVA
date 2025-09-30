7ea5ef2d70ec883531beb02af902394d221a0699
Ahnaf Zahin <mdzahin2005@gmail.com>
2025-09-30 14:30:43 +0600

    Create BubbleSort2.java

---
+++ src/Class_07_Sorting/BubbleSort2.java
@@ -0,0 +1,56 @@
+package Class_07_Sorting;
+
+public class BubbleSort2 {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        //sorted(arr);
+        desSort(arr);
+        //print(arr);
+    }
+    public static void sorted(int[] arr) {
+        //taking the largest number to its correct position
+        int n = arr.length;
+        for(int i=0; i<n-1; i++){
+            int swap = 0;
+            for(int j=0; j<n-1-i; j++){
+                if(arr[j]>arr[j+1]) {
+                    swap(arr, j);
+                    swap++;
+                }
+                print(arr);
+            }
+            if(swap==0) break;
+        }
+    }
+    public static void desSort(int[] arr){
+        //taking the smallest number to its correct position
+        int n = arr.length;
+        for(int i=0; i<n-1; i++){
+            int swap = 0;
+            for(int j=n-1; j>i; j--){
+                if(arr[j]<arr[j-1]) {
+                    desSwap(arr, j);
+                    swap++;
+                }
+                print(arr);
+            }
+            if(swap==0) break;
+        }
+    }
+    public static void desSwap(int[] arr, int j){
+        int temp = arr[j];
+        arr[j] = arr[j-1];
+        arr[j-1] = temp;
+    }
+    public static void swap(int[] arr, int j){
+        int temp = arr[j];
+        arr[j] = arr[j +1];
+        arr[j +1] = temp;
+    }
+    public static void print(int[] arr){
+        for(int ele: arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+    }
+}
