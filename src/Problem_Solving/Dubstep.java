2ddbfa39bde099240b017fa85d54bbd1cb718c27
Ahnaf Zahin <mdzahin2005@gmail.com>
2025-10-25 11:51:28 +0600

    finally solved DUBSTEP!!

--- src/Problem_Solving/Dubstep.java
+++ src/Problem_Solving/Dubstep.java
@@ -6,29 +6,17 @@ public class Dubstep {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        String wub = "WUB";
-        String temp = "";
         String ans = "";
-        int i=0, j=0;
-        while(j<s.length()){
-            if(s.charAt(j) == wub.charAt(i)){
-                temp+=s.charAt(j);
-                if(i==2) {
-                    ans+= temp + " ";
-                    temp = "";
-                    i=0;
-                }
-                else {
-                    i++;
-                }
+        int i=0;
+        while(i<s.length()){
+            if(i<=s.length()-3 && s.substring(i,i+3).equals("WUB")){
+                if(ans.length()>0) ans+= " ";
+                i = i+3;
+            }else{
+                ans+=s.charAt(i);
+                i++;
             }
-            else{
-                i=0;
-                temp+=s.charAt(j);
-            }
-            j++;
         }
-        ans+= temp;
         System.out.println(ans);
     }
 }
