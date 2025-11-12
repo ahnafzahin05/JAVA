558deecfb64661e042f7cc69f6f7149e0b692c07
Ahnaf Zahin <mdzahin2005@gmail.com>
2025-11-12 10:09:22 +0600

    cf practice

---
+++ src/Problem_Solving/MultiplicationTable_577A.java
@@ -0,0 +1,13 @@
+package Problem_Solving;
+
+import java.util.Scanner;
+
+public class MultiplicationTable_577A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int c = 0;
+
+    }
+}
